158b9aecacd68c14c8926fcd5e082753bdddba10
unknown <rraj58361@gmail.com>
2024-02-24 15:36:01 +0530

    UI

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import ForgetPswd from './components/ForgetPswd';
+import ResetPaswd from './components/ResetPaswd';
+import EmailValidation from './components/EmailValidation';
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignUp/>}/>
+        <Route path="/email/verification" element={<EmailValidation/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/pwd" element={<ForgetPswd/>}/>
+        <Route path="/resetpwd" element={<ResetPaswd/>}/>
+      </Routes>
+      </BrowserRouter>
+    
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/UserType.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+export default function BasicSelect() {
+  const [userType, setUserType] = React.useState('');
+
+  const handleChange = (event) => {
+    setUserType(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">UserType</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={userType}
+          label="userType"
+          onChange={handleChange}
+        >
+          <MenuItem value={"admin"}>Admin
+          </MenuItem>
+          <MenuItem value={"user"}>User
+          </MenuItem>
+         
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
